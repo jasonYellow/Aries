6315e00eb1186d715e4cf7d391c95e26ba7be407
jasonYellow <290784252@qq.com>
2020-06-11 10:05:58 +0800

    提交代码和pic

--- src/rfx_encode_pic/rfx_encode.c
+++ src/rfx_encode_pic/rfx_encode.c
@@ -9,8 +9,9 @@ int main()
 	BYTE* pSrcData = NULL;
 	wStream* s;	
 	FILE *f,*fp = NULL;
-	BITMAPFILEHEADER *bitmap_file_header;
-	BITMAPV5HEADER *bitmapv5_header;
+	BITMAPFILEHEADER bitmap_file_header;
+	BITMAPINFOHEADER bitmap_info_header;
+	RGBQUAD bitmap_rgb;
 	int ret;
 	int stride;
 	struct timeval start, end;
@@ -32,42 +33,24 @@ int main()
 	rfx_context_set_pixel_format(context,PIXEL_FORMAT_BGRA32);
 	context->width = 1920;
 	context->height = 1080;
+
 	s = Stream_New(NULL, 0xFFFF);
 
-        bitmap_file_header = malloc(sizeof(BITMAPFILEHEADER));
-	bitmapv5_header = malloc(sizeof(BITMAPV5HEADER));        
+	pSrcData = malloc(1920 * 1080 * 4);
 
-	fp = fopen("/tmp/head","w");
-	f = fopen("/tmp/pic.bmp", "r");
-	ret = fread ( bitmap_file_header, 1, sizeof(BITMAPFILEHEADER), f);
-	ret = fwrite ( bitmap_file_header, sizeof(BITMAPFILEHEADER),1, fp);
-	printf("bfSize:%d,bfOffBits:%d\n",bitmap_file_header->bfSize,bitmap_file_header->bfOffBits);
-	ret = fread ( bitmapv5_header, 1, sizeof(BITMAPV5HEADER), f);
-	ret = fwrite ( bitmapv5_header, sizeof(BITMAPV5HEADER),1, fp);
-	printf("bV5Width:%d,bV5Height:%d,bV5SizeImage:%d\n",bitmapv5_header->bV5Width,bitmapv5_header->bV5Height,bitmapv5_header->bV5SizeImage);
+	fp = fopen("/tmp/0.bmp","rb");
+	ret = fread ( &bitmap_file_header, 1, sizeof(BITMAPFILEHEADER), fp);
+	ret = fread ( &bitmap_info_header, 1, sizeof(BITMAPINFOHEADER), fp);
+	ret = fread ( &bitmap_rgb, 1, sizeof(RGBQUAD), fp);
 	fclose(fp);
 
 
 	rect.x = 0;
 	rect.y = 0;
-	rect.width = bitmapv5_header->bV5Width;
-	rect.height = bitmapv5_header->bV5Height;
+	rect.width = 1920;
+	rect.height = 1080;
 
-	stride = bitmapv5_header->bV5Width * 4;
-
-	printf("fread\n");
-
-        pSrcData = malloc(bitmapv5_header->bV5SizeImage);
-	//memcpy(pSrcData,(BYTE*)refImage,IMG_WIDTH * IMG_HEIGHT * FORMAT_SIZE);
-        //pSrcData = (BYTE*)refImage;
-	ret = fread ( pSrcData, bitmapv5_header->bV5SizeImage, 1, f);
-        fclose(f);
-
-	f = fopen("/tmp/rgba.data", "w");
-	ret = fwrite ( pSrcData, bitmapv5_header->bV5SizeImage , 1, f);
-        fclose(f);
-
-        printf("fwrite\n");
+	stride = 1920 * 4;
 
 	gettimeofday(&start, NULL);
 	if (!(rfx_compose_message(context, s, &rect, 1, pSrcData, rect.width, rect.height, stride)))
